dabaf24233cd34b7252fc59e574e304e23dff853
Patrick Bastek <11slendermenn11@gmail.com>
2022-07-19 19:39:43 +0200

    Added brace support.

--- calc.nim
+++ calc.nim
@@ -28,34 +28,45 @@ proc calc(expr : string) : float =
 
   var ex : string = expr
 
+  if '(' in ex and (not (')' in ex)) or (not ('(' in ex)) and ')' in ex:
+    echo "Error, no closed braces"
+    return 0.00
+
   if '(' in ex and ')' in ex:
-    var
-      pos1 :int = ex.find('(')
-      pos2 :int = len ex
-    pos2 -= 1
 
-    while pos2 > pos1:
-      if ex[pos2] == ')':
-        break
-      pos2 -= 1
+    while '(' in ex and ')' in ex:
 
-    pos1 += 1
-    pos2 -= 1
-
-    var temp = ex[pos1..pos2]
-    if ')','(' in temp:
-      var i = temp.find(')')
+      var
+        braces : seq[int] = @[]
+        braceCnt : int = 0
 
 
-    let num : float = calc(ex[pos1..pos2])
-    
-    ex = ex[0..pos1-2] &  fmt("{num}") & ex[pos2+2..^1]
+      for idx, s in ex[0..^1]:
+        if s == '(':
+          braces.add(idx+1)
+          braceCnt += 1
+        elif(s == ')'):
+          braces.add(-1*(idx+1))
+          braceCnt -= 1
+
+      if braceCnt != 0:
+        echo ("Error, no closed braces")
+        return 0.00
+
+      for idx in 0 .. braces.len-2:
+
+        if braces[idx] > 0 and braces[idx+1] < 0:
+          var
+            pos1 = braces[idx]-1
+            pos2 = (braces[idx+1]*(-1))-1
+
+          let num = calc(ex[pos1+1..pos2-1])
+
+          ex[pos1..pos2] = fmt("{num}")
+          break
 
 
 
-
-
-      
   if not('*' in ex or '/' in ex or '+' in ex or '-' in ex):
     var res : float
     discard parsefloat(ex, res)
